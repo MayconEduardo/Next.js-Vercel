8d338c427b6ca5267c73f2d6806398509f0325a1
MayconEduardo <mayconeduardo16@gmail.com>
2021-01-09 15:55:10 -0300

    Primeiro Teste

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div>
+            <h1>Família</h1>
+            <div>Maycon Eduardo Gonçalves da Silva</div>
+            <div>Alexsandra Paula Passos da Silva</div>
+            <div>Yan Eduardo Passos da Silva</div>
+        </div>
+    )
+}
+
+export default Home
