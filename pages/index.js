0a6c4c1baac1de35e89edefd74ef425b5ff6d3c8
MayconEduardo <maycon.silva@havan.com.br>
2021-01-26 08:26:01 -0300

    Hello World

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 function Home() {
     return (
         <div>
-            <h1>Família</h1>
-            <div>Maycon Eduardo Gonçalves da Silva</div>
-            <div>Alexsandra Paula Passos da Silva</div>
-            <div>Yan Eduardo Passos da Silva</div>
+            <h1>Hello World</h1>
         </div>
     )
 }
